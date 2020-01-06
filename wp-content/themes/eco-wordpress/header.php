a407dd4fd403310254bfd24bc0d029c30b3d85cf
Jeremy <jeremybrunet86470@gmail.com>
2020-01-06 16:26:03 +0100

    modif

--- wp-content/themes/eco-wordpress/header.php
+++ wp-content/themes/eco-wordpress/header.php
@@ -15,6 +15,7 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
 	<link  href="<?php echo get_template_directory_uri() ?>/js/viewer.js/viewer.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Mukta&display=swap" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
